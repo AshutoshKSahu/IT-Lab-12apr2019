24eb28f8cb27affcbf688b29a58f703b6a04b63a
Ashutosh Sahu <47266286+ashu21sahu@users.noreply.github.com>
2019-04-12 12:41:15 +0530

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,42 @@
+<?php
+$servername="localhost";
+$username="ashu";
+$password="root";
+$dbname="2016cse399";
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+if(!$conn)
+{
+	die("Connetion failed:".mysqli_connect_error());
+}
+$sql="select * from employees";
+if($res=mysqli_query($conn,$sql))
+{
+	if(mysqli_num_rows($res)>0)
+	{
+		echo "<table>";
+		echo "<tr>";
+		echo "<th>FirstName</th>";
+		echo "<th>LastName</th>";
+		echo "<th>Age</th>";
+		echo "</tr>";
+		while($row=mysqli_fetch_array($res))
+		{
+			echo "<tr>";
+			echo "<td>".$row["firstname"]."</td>";
+			echo "<td>".$row["lastname"]."</td>";
+			echo "<td>".$row["age"]."</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+	}
+	else
+	{
+		echo "No matching records are found!";
+	}
+}
+else
+{
+	echo "ERROR:Could not able to execute $sql.".mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
